030cdbf3d0c07d830c0d895835c253699b36d2a0
nandanugg <nandanugg@gmail.com>
2025-01-12 12:41:22 +0700

    fix file test file size and type

--- src/scenario/fileTest.js
+++ src/scenario/fileTest.js
@@ -49,7 +49,7 @@ export function UploadFileTest(user, config, tags) {
       {
         ["should return 400"]: (res) => res.status === 400,
       },
-      ["noContentType"], config, tags,);
+      [], config, tags,);
     assertHandler(
       "invalid file size", featureName, route,
       {
@@ -59,7 +59,7 @@ export function UploadFileTest(user, config, tags) {
       {
         ["should return 400"]: (res) => res.status === 400,
       },
-      ["noContentType"], config, tags,
+      [], config, tags,
     );
     assertHandler(
       "invalid content type", featureName, route, positivePayload, positiveHeader,
