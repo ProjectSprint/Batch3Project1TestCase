0906fcf836ff5be4609d0f10f84315f1341b46e2
nandanugg <nandanugg@gmail.com>
2025-01-12 15:26:09 +0700

    fix file loading path

--- main.js
+++ main.js
@@ -10,10 +10,10 @@ export const options = {
   iterations: 1
 };
 
-const smallFile = open('../figure/image-50KB.jpg', 'b');
-const medFile = open('../figure/image-100KB.jpg', 'b');
-const bigFile = open('../figure/image-200KB.jpg', 'b');
-const invalidFile = open('../figure/sql-5KB.sql', 'b');
+const smallFile = open('./src/figure/image-50KB.jpg', 'b');
+const medFile = open('./src/figure/image-100KB.jpg', 'b');
+const bigFile = open('./src/figure/image-200KB.jpg', 'b');
+const invalidFile = open('./src/figure/sql-5KB.sql', 'b');
 
 export default function() {
   /** @type {import("./src/types/config.js").Config} */
